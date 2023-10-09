f3aad4f8e437ab75290504503984bada475fe3f9
ignaciokriche <ignaciokriche@gmail.com>
2023-10-09 16:11:35 -0300

    Memory Arithmetic.

--- src/test/java/ar/com/kriche/MemoryArithmeticTest.java
+++ src/test/java/ar/com/kriche/MemoryArithmeticTest.java
@@ -20,20 +20,15 @@ public class MemoryArithmeticTest {
         MemoryArithmetic<Integer> actualArrayRecursive = new MemoryArithmetic<Integer>(X, Y, Z, T);
         MemoryArithmetic<Integer> actualArrayIterative = new MemoryArithmetic<Integer>(X, Y, Z, T);
 
-        System.out.println("Loading arrays...");
-
         int expectedValue = 1;
         for (int x = 0; x < X; x++)
             for (int y = 0; y < Y; y++)
                 for (int z = 0; z < Z; z++)
                     for (int t = 0; t < T; t++) {
-                        System.out.println("setting coordinate (" + x + "," + y + "," + z + "," + t + ") to store: " + expectedValue + ".");
                         actualArrayRecursive.setValueAtRecursive(expectedValue, x, y, z, t);
                         actualArrayIterative.setValueAtIterative(expectedValue, x, y, z, t);
                         expectedValue++;
                     }
-
-        System.out.println("Verifying arrays...");
 
         int actualValueRecursive, actualValueIterative;
         expectedValue = 1;
@@ -41,7 +36,6 @@ public class MemoryArithmeticTest {
             for (int y = 0; y < Y; y++)
                 for (int z = 0; z < Z; z++)
                     for (int t = 0; t < T; t++) {
-                        System.out.println("expecting coordinate (" + x + "," + y + "," + z + "," + t + ") to be: " + expectedValue + ".");
                         actualValueRecursive = actualArrayRecursive.getValueAtRecursive(x, y, z, t);
                         actualValueIterative = actualArrayIterative.getValueAtIterative(x, y, z, t);
                         if (!(actualValueRecursive == actualValueIterative && expectedValue == actualValueRecursive))
@@ -49,8 +43,6 @@ public class MemoryArithmeticTest {
                                     expectedValue + "; iterative: " + actualValueIterative + "; recursive: " + actualValueRecursive + ";");
                         expectedValue++;
                     }
-
-        System.out.println("All good ;)");
 
     }
 
