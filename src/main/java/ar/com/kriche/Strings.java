c16e933efc9d36303dd815824d8c1090c1d6bf3a
ignaciokriche <ignaciokriche@gmail.com>
2023-11-27 12:43:05 -0300

    string exercises.

--- src/main/java/ar/com/kriche/Strings.java
+++ src/main/java/ar/com/kriche/Strings.java
@@ -459,33 +459,5 @@ public class Strings {
         }
         return -1;
     }
-
-    /**
-     * @param numbers
-     * @return the subarray with the biggest sum.
-     */
-    public static int[] maxSubArraySum(@NotNull int[] numbers) {
-        if (numbers.length == 0) {
-            return new int[]{};
-        }
-        // Kadane’s Algorithm:
-        int maxEndingHere = 0;
-        int maxSum = Integer.MIN_VALUE;
-        int maxSumLeftIndex = 0, maxSumRightIndex = 0, currentSumIndex = 0;
-        for (int i = 0; i < numbers.length; i++) {
-            maxEndingHere += numbers[i];
-            if (maxEndingHere > maxSum) {
-                maxSum = maxEndingHere;
-                maxSumLeftIndex = currentSumIndex;
-                maxSumRightIndex = i;
-            }
-            if (maxEndingHere < 0) {
-                currentSumIndex = i + 1;
-                maxEndingHere = 0;
-            }
-        }
-        return Arrays.copyOfRange(numbers, maxSumLeftIndex, maxSumRightIndex + 1);
-    }
-
-
+    
 }
