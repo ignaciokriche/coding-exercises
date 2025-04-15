ff9e0119b86b546b89f6555d537940ba727ca780
ignaciokriche <ignaciokriche@gmail.com>
2025-04-15 19:06:58 -0300

    recursively printing an integer.

--- src/test/java/ar/com/kriche/recursion/RecursiveIntegerPrinterTest.java
+++ src/test/java/ar/com/kriche/recursion/RecursiveIntegerPrinterTest.java
@@ -2,19 +2,20 @@ package ar.com.kriche.recursion;
 
 import org.junit.jupiter.api.Test;
 
+import static ar.com.kriche.recursion.RecursiveIntegerPrinter.printNum;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 /**
  * @author Kriche
  */
-class RecursiveIntegerPrinterTest {
+public class RecursiveIntegerPrinterTest {
 
     @Test
-    void printNumTest() {
+    public void printNumTest() {
         final int min = -5000000;
         final int max = -min;
         for (int i = min; i <= max; i++) {
-            assertEquals(String.valueOf(i), RecursiveIntegerPrinter.printNum(i));
+            assertEquals(String.valueOf(i), printNum(i));
         }
     }
 
